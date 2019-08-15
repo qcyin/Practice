0323e8fa778e845e890b6a3c243b84e6cd30eea0
yinqicheng <785128850@qq.com>
2019-08-15 23:01:51 +0800

    mybayis

--- src/main/scala/com/com/MybatisTest.java
+++ src/main/scala/com/com/MybatisTest.java
@@ -1,6 +1,8 @@
 package com.com;
 
+import com.com.bean.Orders;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -9,9 +11,11 @@ import java.io.InputStream;
 public class MybatisTest {
 
     public static void main(String[] args) throws Exception {
-        ;
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        Orders orders = session.selectOne("com.com.bean.Orders.getOrdersById", 1);
+
     }
 }
